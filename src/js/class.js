d44c29c1725ec27662b0009688b9a3bf86c90718
annaboye <boye.anna@gmail.com>
2022-11-02 16:24:31 +0100

    removed error code in class.js

--- src/js/class.js
+++ src/js/class.js
@@ -4,19 +4,3 @@ export class ListItem {
     this.checked = checked;
   }
 }
-if (e.target.checked) {
-  theCompletedList.push(theTask);
-  for (let i = 0; i < theCompletedList.length; i++) {
-    console.log(theCompletedList);
-    theList.splice(theTask, 1);
-    let labelTag = document.createElement("label");
-    let checkBox = document.createElement("input");
-    let listTag = document.createElement("li");
-    checkBox.classList.add("clickable");
-    checkBox.type = "checkbox";
-    labelTag.innerText = theCompletedList[i].title;
-    listTag.appendChild(checkBox);
-    listTag.appendChild(labelTag);
-    completedTag.appendChild(listTag);
-  }
-}
