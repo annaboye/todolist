3830366248138652d387967e7a217ceb103d7204
annaboye <boye.anna@gmail.com>
2022-11-02 16:16:59 +0100

    completedlist working

--- src/js/main.js
+++ src/js/main.js
@@ -11,32 +11,37 @@ function addtolist() {
   let newTask = inTag.value;
   newItemObject = new ListItem(newTask, false);
   theList.push(newItemObject);
+  inTag.value = "";
+
   innerTag();
-  newTask = " ";
 }
 
 function innerTag() {
-  let ulTag = document.getElementById("todolist");
-  ulTag.innerHTML = "";
+  let todoTag = document.getElementById("todolist");
+  todoTag.innerHTML = "";
   for (let i = 0; i < theList.length; i++) {
     let labelTag = document.createElement("label");
     let checkBox = document.createElement("input");
     let listTag = document.createElement("li");
-    checkBox.classList.add("clickable");
     checkBox.type = "checkbox";
     labelTag.innerText = theList[i].title;
-
-    checkBox.addEventListener("click", () => {
-      completedTask(theList[i]);
-    });
     listTag.appendChild(checkBox);
     listTag.appendChild(labelTag);
-    ulTag.appendChild(listTag);
+
+    checkBox.addEventListener("change", (e) => {
+      completedTask(e, listTag, todoTag);
+    });
+    todoTag.appendChild(listTag);
   }
 }
 
-function completedTask(task) {
-  console.log("du klickade på", task);
+function completedTask(e, listTag, todoTag) {
+  let completedTag = document.getElementById("completed");
+  if (e.target.checked) {
+    completedTag.appendChild(listTag);
+  } else {
+    todoTag.appendChild(listTag);
+  }
 }
 
 function init() {
